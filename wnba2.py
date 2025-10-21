d6ad341d028048c14b756baeb52a160cb1d9c7a0
Swansb306 <overlord22567@gmail.com>
2025-10-21 19:23:04 -0400

    updated organization, added comments

--- wnba2.py
+++ wnba2.py
@@ -5,6 +5,7 @@ pip install pandas numpy matplotlib scikit-learn statsmodels
 @author: bswan work
 """ 
 'This project is to try to predict winners of wnba games.'
+#note, this is still a work in progress.
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,27 +13,42 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import make_pipeline
+
+#####load the data
+
 pd.set_option('display.max_columns', None)
-df = pd.read_csv("C:\\Users\\bswan work\\Downloads\\WNBA-stats-master\\WNBA-stats-master\\wnba-team-elo-ratings.csv")
+df = pd.read_csv("wnba-team-elo-ratings.csv")
 """df.rename(columns={"Date": "date", "Numbers": "check_ins"}, inplace=True)"""
 
-df2 = pd.read_csv("C:\\Users\\bswan work\\Downloads\\WNBA-stats-master\\WNBA-stats-master\\wnbaoffv1.csv")
+df2 = pd.read_csv("wnbaoffv1.csv")
 
 """
 df2 = df2.merge(df[['prob1','elo1_pre','elo2_pre']], 
               left_on=['date', 'team1', 'team2'], right_on=['date', 'team1', 'team2'], 
               how='left')
 """
-
+#this is for making our Dependent variables
 df['point_diff'] = df.score1 - df.score2
 df['team1win'] = np.where(df['point_diff'] > 0, 1, 0)
 
+"""  This is with your new measure of offense
+"""
+df2['point_diff'] = df2.score1 - df2.score2
+df2['team1win'] = np.where(df2['point_diff'] > 0, 1, 0)
 """if df['point_diff'] > 0 :
     df['team1win'] = 1 
 else: df['team1win'] = 0"""
 
-"logit model base model"
 
+
+
+
+####Descriptive Statistics
 "lets do some descriptive statistics"
 import seaborn as sns
 print(df.describe())
@@ -42,7 +58,16 @@ df.value_counts()
 _ = df.hist(figsize=(20, 14))
 df.isnull().sum()
 
+"some quick descriptive stats for df2"
+_ = df2.hist(figsize=(20, 14))
+df2.isnull().sum()
+print(df2.describe())
+print("value counts")
+df2.value_counts()
 
+
+
+###prep for modeling with test and train split
 "start modeling"
 train = df.copy()
 test = df.copy()
@@ -57,30 +82,19 @@ y_train = train['team1win']
 X_test = test[['elo1_pre','elo2_pre']]
 y_test = test['team1win']
 
-
+#models
+"logit model base model"
 model = LogisticRegression(solver='liblinear', random_state=0)
 result = model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 predicted = model.predict_proba(X_test)[:,1]
 "print(y_pred.summary())"
 
-"Wierdly the below gives you a summary, what you had before would not"
+#this gives you a summary because sklearn doesn't have an easy summary option. 
 logit_model = sm.Logit(y, X)
 result = logit_model.fit()
 print(result.summary())
 
-
-"""  This is with your new measure of offense
-"""
-df2['point_diff'] = df2.score1 - df2.score2
-df2['team1win'] = np.where(df2['point_diff'] > 0, 1, 0)
-
-"some quick descriptive stats"
-_ = df2.hist(figsize=(20, 14))
-df2.isnull().sum()
-print(df2.describe())
-print("value counts")
-df2.value_counts()
 
 """
 x = sns.scatterplot(
@@ -92,6 +106,7 @@ x = sns.scatterplot(
 
 plt.show(x)"""
 
+#df2 test train split
 train = df2.copy()
 test = df2.copy()
 train = train.loc[train['season'] < 2016]
@@ -106,7 +121,7 @@ X_test = test[['elo1_pre','elo2_pre','team1_offense_metric','team2_offense_metri
 y_test = test['team1win']
 
 'this is the model in sklearn'
-
+#df2 modeling
 model = LogisticRegression(solver='liblinear', random_state=0)
 result = model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
@@ -123,7 +138,7 @@ summary = pd.DataFrame({
     'Odds Ratio': np.exp(model.coef_[0])
 })
 
-# Add Intercept (optional)
+# Add Intercept 
 intercept = pd.DataFrame({
     'Feature': ['Intercept'],
     'Coefficient': [model.intercept_[0]],
@@ -145,9 +160,8 @@ print(sm_result.summary())
 prediction1 = sm_result.predict(X_test2)
 
 
-
-"""df2024 = pd.read_csv("C:\\Users\\bswan work\\Documents\\python quant project\\Python codes\\Python codes\\wnba2024.csv", header = 1)"""
-df2024 = pd.read_csv("C:\\Users\\bswan work\\Documents\\python quant project\\Python codes\\Python codes\\wnba2024dvoa2.csv")
+##### 2024 data
+df2024 = pd.read_csv("wnba2024dvoa2.csv")
 
 
 """df2024.rename(columns={"PTS": "score1", "PTS.2": "score2"}, inplace=True)
@@ -170,9 +184,9 @@ predicted = model2024.predict_proba(newx)[:,1]
 accuracy = model2024.score(newx, newy)
 print(f"Accuracy of logistic regression: {accuracy:.3f}")
 
-'okay time for 2025 data!'
+#############'okay time for 2025 data!'
 
-df2025 = pd.read_csv("C:\\Users\\bswan work\\Documents\\python quant project\\Python codes\\Python codes\\wnba2025withoffense_june11.csv")
+df2025 = pd.read_csv("wnba2025withoffense_june11.csv")
 
 
 df2025['point_diff'] = df2025.score1 - df2025.score2
@@ -193,11 +207,12 @@ predicted = model2025.predict_proba(newx)[:,1]
 accuracy = model2025.score(newx, newy)
 print(f"Accuracy of logistic regression: {accuracy:.3f}")
 
+
+###### at this point we are trying to use the model to predict for with new data
+#####When I was first working on it this was during week11 of the season. 
+
 "week 11"
-df2025week11 = pd.read_csv("C:\\Users\\bswan work\\Documents\\python quant project\\Python codes\\Python codes\\2025forwardpredictions\\weekofjune11.csv")
-
-
-
+df2025week11 = pd.read_csv("weekofjune11.csv")
 "newy = df2025week11['team1win']"
 newx = df2025week11[['team1_offense_metric','team2_offense_metric']]
 
@@ -235,7 +250,7 @@ _ = sns.pairplot(
     diag_kind="hist",
     diag_kws={"bins": 30},
 )"""
-
+########more data exploration using seaborn for df2
 _ = sns.scatterplot(
     x="elo1_pre",
     y="team1_offense_metric",
@@ -255,12 +270,10 @@ sns.heatmap(correlationmatrix1, annot=True, cmap='coolwarm', fmt=".2f")
 plt.title('Correlation Matrix Heatmap')
 plt.show()
 
+
+####exploring decision trees and other alternative methods/models
 from sklearn.model_selection import cross_validate
 from sklearn.tree import DecisionTreeRegressor
-
-
-
-
 
 target = df2['team1win']
 
@@ -325,10 +338,6 @@ print(
     f"{scores.mean():.3f} ± {scores.std():.3f}"
 )
 
-from sklearn.linear_model import Ridge
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.pipeline import make_pipeline
 
 
 polynomial_regressor = make_pipeline(
@@ -349,16 +358,17 @@ _ = bagging.fit(data_train, target_train)
 sns.scatterplot(
     x=data_train['team1_offense_metric'], y=target_train, color="black", alpha=0.5
 )
-
+#This previous one is not very informative.
 sns.scatterplot(
     x=data_train['team1_offense_metric'], y=data_train['elo1_pre'], color="black", alpha=0.5
 )
+print(data_train.columns)
 
 sns.scatterplot(
-    x=data_train['team1_offense_metric'], y=data_train['score1'], color="black", alpha=2
+    x=data_train['team1_offense_metric'], y=data_train['point_diff'], color="black", alpha=1
 )
-'not sure what x is supposed to be here...hmm'
-
+'Positive correlation here but it is noisy'
+###########Bagging and Decision Trees
 bagged_trees = BaggingRegressor(
     estimator=DecisionTreeRegressor(max_depth=3),
     n_estimators=100,
@@ -373,3 +383,7 @@ plt.plot(
     label="Predictions of ensemble",
 )
 _ = plt.legend()
+
+print(bagged_trees_predictions)
+
+######end of regular season evaluation.... how did we do? 
